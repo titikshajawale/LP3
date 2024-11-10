1941e3fae5fb47004df6fc89b59dfcba71ca3105
titikshajawale <128623745+titikshajawale@users.noreply.github.com>
2024-11-10 23:04:45 +0530

    Update DAA-4-knapsack.py

--- DAA-4-knapsack.py
+++ DAA-4-knapsack.py
@@ -26,3 +26,24 @@ def solve_knapsack():
 
 if __name__=="__main__":
     solve_knapsack()
+    ----------------------------------------------------------------------------------------------------------------------
+    def knapsack_dp(w, wt , val , n):
+    k = [[0 for x in range(w+1)] for x in range(n+1)]
+
+    for i in range(n+1):
+        for j in range(w+1):
+            if i==0 or j==0:
+                k[i][j] = 0
+            elif wt[i-1] <=j:
+                k[i][j] = max(val[i-1] + k[i-1][j-wt[i-1]] , k [i-1][j])
+            else:
+                k[i][j] = k[i-1][j]
+    return k[n][w]
+
+
+val = [100,60,120]
+wt = [20,10,30]
+w = 50
+n = len(val)
+print(knapsack_dp(w, wt , val , n))
+
